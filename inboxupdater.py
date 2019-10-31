b8d09d81c34df75c3b2075b8019fef3322e7836d
Gez Quinn <holla@gezquinn.design>
2019-10-31 23:24:25 +0000

    updater

--- inboxupdater.py
+++ inboxupdater.py
@@ -1,17 +1,17 @@
 import json, requests, uuid, todoist
 
 token = "dd7679bad6a4558c8e23f753d3f30adf31008dc2";
-projects = requests.get("https://beta.todoist.com/API/v8/projects", params={"token": token}).json()
+projects = requests.get("https://api.todoist.com/rest/v1/projects", params={"token": token}).json()
 dict = projects
 inboxId = [obj.get('id') for obj in projects if(obj['name'] == 'Inbox')]
 nowId = [obj.get('id') for obj in projects if(obj['name'] == 'NOW')]
 filter = "no date"
 
-tasks = requests.get("https://beta.todoist.com/API/v8/tasks", params={"token": token, "project_id": inboxId, "filter": filter}).json()
+tasks = requests.get("https://api.todoist.com/rest/v1/tasks", params={"token": token, "project_id": inboxId, "filter": filter}).json()
 
 for task in tasks:
     taskId = task.get('id')
-    requests.post("https://beta.todoist.com/API/v8/tasks/%s" % taskId,
+    requests.post("https://api.todoist.com/rest/v1/tasks/%s" % taskId,
         params={"token": token},
         data=json.dumps({"due_string": "today"}),
         headers={
@@ -20,11 +20,12 @@ for task in tasks:
         }
     )
 
-tasks = requests.get("https://beta.todoist.com/API/v8/tasks", params={"token": token, "project_id": inboxId}).json()
+tasks = requests.get("https://api.todoist.com/rest/v1/tasks", params={"token": token, "project_id": inboxId}).json()
 for task in tasks:
     taskId = task.get('id')
     api = todoist.TodoistAPI(token)
     api.sync()
     item = api.items.get_by_id(taskId)
-    item.move(nowId[0])
+    print(nowId[0])
+    item.move(project_id=nowId[0])
     api.commit()
