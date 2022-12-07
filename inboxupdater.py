7e7fb67dcce5aaa0a10308a41f24928235d47dab
Gez Quinn <holla@gezquinn.design>
2022-12-07 15:05:13 +0000

    feat: use v9 sync api and requests module

--- inboxupdater.py
+++ inboxupdater.py
@@ -1,33 +1,39 @@
 import json, requests, uuid, os
 
-from todoist.api import TodoistAPI
-
 token = os.environ['API_TOKEN']
 destination = os.environ['DESTINATION_PROJECT']
-projects = requests.get("https://api.todoist.com/rest/v1/projects", params={"token": token}).json()
-dict = projects
-inboxId = [obj.get('id') for obj in projects if(obj['name'] == 'Inbox')]
-nowId = [obj.get('id') for obj in projects if(obj['name'] == destination)]
-filter = "no date"
 
-tasks = requests.get("https://api.todoist.com/rest/v1/tasks", params={"token": token, "project_id": inboxId, "filter": filter}).json()
+projectsResponse = requests.post("https://api.todoist.com/sync/v9/sync", data={"sync_token":"*", "resource_types":'["projects"]'}, headers={"Authorization": "Bearer "+token}).json()
+projects = projectsResponse['projects']
+
+inboxId = [obj.get('id') for obj in projects if(obj['name'] == "Inbox")][0]
+nowId = [obj.get('id') for obj in projects if(obj['name'] == destination)][0]
+
+tasksResponse = requests.post("https://api.todoist.com/sync/v9/projects/get_data", data={"project_id":inboxId}, headers={"Authorization": "Bearer "+token}).json()
+
+tasks = tasksResponse['items']
+
+commands = []
 
 for task in tasks:
     taskId = task.get('id')
-    requests.post("https://api.todoist.com/rest/v1/tasks/%s" % taskId,
-        params={"token": token},
-        data=json.dumps({"due_string": "today"}),
-        headers={
-            "Content-Type": "application/json",
-            "X-Request-Id": str(uuid.uuid4()),
+    commands.append({
+        "type": "item_update", 
+        "uuid": str(uuid.uuid4()), 
+        "args": {
+            "id": taskId, 
+            "due": {"string": "today"}
         }
-    )
+    })
+    commands.append({
+        "type": "item_move",
+        "uuid": str(uuid.uuid4()), 
+        "args": {
+            "id": taskId, 
+            "project_id": nowId
+        }
+    })
 
-tasks = requests.get("https://api.todoist.com/rest/v1/tasks", params={"token": token, "project_id": inboxId}).json()
-for task in tasks:
-    taskId = task.get('id')
-    api = TodoistAPI(token)
-    api.sync()
-    item = api.items.get_by_id(taskId)
-    item.move(project_id=nowId[0])
-    api.commit()
+batchResponse = requests.post("https://api.todoist.com/sync/v9/sync", data={"commands":json.dumps(commands)}, headers={"Authorization": "Bearer "+token}).json()
+
+print(batchResponse)
